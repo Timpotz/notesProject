f41c0a019550612e033f8c2c9b8060fbe641698c
Timpotz <timotiusp187@gmail.com>
2022-12-15 14:43:41 +0700

    change port 8000

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes= require('./routes')
 const init = async () => {
     //server.options routes{cors{origin:[]}} to allow same origin access for whole server
     const server = Hapi.server({
-        port: 5000,
+        port: 8000,
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
           cors: {
